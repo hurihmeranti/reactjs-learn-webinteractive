d78cdf257d4048688aaeea3d597257ce4582c664
hurihmeranti <hurihmeranti@gmail.com>
2023-03-02 22:13:42 +0700

    phase-2 logi243

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState("home");
+    const [scolled, seScrolled] = useState(false);
 
- return(
-   <Navbar expand ="lg">
+    useEffect(() => {
+        const onScroll = () => {
+            if(window.scrollY > 50){
+                seScrolled(true);
+            } else {
+                seScrolled(false);
+            }
+        } 
+
+        window.addEventListener("scroll", onScroll);
+
+        return() => window.removeEventListener("scroll", onscroll);
+    }, []);
+
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
+    return(
+   <Navbar expand ="lg" className={scrolled ? "scrolled": ""}>
    <Container>
    <Navbar.Brand href="#home">
         <img src={""} alt="logo company" />
@@ -15,9 +35,9 @@ export const NavBar = () => {
    </Navbar.Toggle>
    <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="me-auto">
-    <Nav.Link href="#home">Home</Nav.Link>
-    <Nav.Link href="#about">About</Nav.Link>
-    <Nav.Link href="#Project">Project</Nav.Link>   
+    <Nav.Link href="#home" className={activeLink === "home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("home")}>Home</Nav.Link>
+    <Nav.Link href="#about" className={activeLink === "about" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("about")}>About</Nav.Link>
+    <Nav.Link href="#Project" className={activeLink === "project" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("project")}>Project</Nav.Link>   
     </Nav>
     <span className="navbar-text">
         <div className="social-icon">
